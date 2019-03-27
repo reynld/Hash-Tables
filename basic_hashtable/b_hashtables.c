bac2fec9ffb3a67f584d8686e61cda0b109639c5
Chris Cedeno <chrisrey95@gmail.com>
2019-03-27 13:39:27 -0400

    hash table insert

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -72,8 +72,7 @@ BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht = malloc(sizeof(BasicHashTable));
   ht->capacity = capacity;
-  ht->storage = calloc(capacity, sizeof(char *));
-
+  ht->storage = calloc(capacity, sizeof(Pair));
   return ht;
 }
 
@@ -86,6 +85,10 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  Pair *pr = create_pair(key, value);
+  
+  int hk = hash(key, ht->capacity);
+  ht->storage[hk] = pr;
 
 }
 
